451b93a5f269d6a94fa6d5445dd7d1bfd39d92fb
klode <claudia@atomicsoftware.com>
2016-11-17 16:09:49 -0500

    remove dead code

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//require("./sass/main.scss");
 require("./sass/demo.scss");
 
 ReactDOM.render(<App />, document.getElementById('app'));
